536aec44d550effcbbc902330f63cd82d54d752e
Franklin <fwillemen@protonmail.com>
2025-07-05 10:52:11 +0200

    containerized express app

---
+++ containerize_express_app/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const users = [];
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+// register new user
+app.post('/users', (req, res) => {
+    const newUserId = req.body.userId;
+    if (!newUserId) {
+        return res.status(400).send('Missing userId');
+    }
+
+    if (users.includes(newUserId)) {
+        return res.status(400).send('userId already exists.');
+    }
+
+    users.push(newUserId);
+    return res.status(201).send('User registered')
+
+});
+
+// get registered users
+app.get('/users', (req, res) => {
+    return res.json({users});
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
