cba3a8f698073ef13b77edbcac9d368bfa776bc5
Franklin <fwillemen@protonmail.com>
2025-07-06 12:27:41 +0200

    production only

--- optimizing-images/index.js
+++ optimizing-images/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Image Optimization Service!');
+});
+
+app.listen(3000, () => {
+    console.log('Image Optimization Service is running on http://localhost:3000');
+});
