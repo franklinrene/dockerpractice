18c340d8d8a5a27f2b0bab8cc5706f10cbbd6685
Franklin <franklinrene@protonmail.com>
2025-07-07 10:40:56 +0200

    aanpassingen tests

--- containerize-react-app/src/App.tsx
+++ containerize-react-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited
+          Blue app
         </p>
         <a
           className="App-link"
